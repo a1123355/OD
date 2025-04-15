fa0643726451f5b9b1fef3d06a16bc955366b3e4
a1123355 <a1123355@mail.nuk.edu.tw>
2025-04-15 10:44:22 +0800

    add curve.php

---
+++ Longjing-age-curvechart.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>台中市龍井區 A11-A33分布</title>
+</head>
+<body>
+
+    <iframe src="https://plotdb.io/v/chart/37095" width="100%" height="600px" allowfullscreen="true" frameborder="0"></iframe>
+</body>
+</html>
